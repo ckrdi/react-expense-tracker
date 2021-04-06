ac04c9b709291e5eb021e0384215c587dfc2ff9a
ckrdi <agung.cokorda@gmail.com>
2021-04-06 12:35:25 +0800

    create actions for context and reducer

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const AddTransaction = () => {
   const [text, setText] = useState('')
   const [amount, setAmount] = useState(0)
+
+  const { addTransaction } = useContext(GlobalContext)
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Date.now(),
+      text,
+      amount: +amount
+    }
+    addTransaction(newTransaction)
+  }
   return (
     <>
       <h3>Add new transaction</h3>
-      <form >
+      <form onSubmit={submitHandler}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,7 +1,11 @@
+import {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
 const Transaction = ({ transaction }) => {
+  const {deleteTransaction} = useContext(GlobalContext)
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text} <span>{transaction.amount < 0 ? '-' : '+'}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+      {transaction.text} <span>{transaction.amount < 0 ? '-' : '+'}${Math.abs(transaction.amount)}</span>
+      <button onClick={()=>deleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>
   )
 }
